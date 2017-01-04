2523bddfe21ee7729fe6a9e6051cef430f6a53c8
Sean Purcell <iburinoc@gmail.com>
2017-01-04 02:13:10 -0500

    added more stuff to test

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-	return 'Hello World!'
+@app.route("/hook", methods=['GET', 'POST'])
+def hook():
+	if request.method == 'GET':
+		return hook_get(request.args)
+	else:
+		return hook_post(request.args)
+
+def hook_get(args):
+	if args.get('hub.verify_token') != 'test_token':
+		return 'Invalid verification token', 403
+	return args.get('hub.challenge'), 200
+
+def hook_post(args):
+	print args
+	return '', 200
 
 if __name__ == '__main__':
 	app.run(port=12000, debug=True)
