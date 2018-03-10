d05088c6629bcd8f8699e1649eafb8b2ca4a46d2
Sean Purcell <me@seanp.xyz>
2018-03-09 19:57:39 -0500

    Switch to docker and get ready to add extension

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import sys
+
+import server
+import sender
+
+def main():
+    if len(sys.argv) < 2:
+        print('Pass in ACCESS_TOKEN as first parameter')
+        return
+
+    send = sender.Sender(sys.argv[1], simple=True)
+    server.send = send
+
+    try:
+        server.app.run('0.0.0.0', port=8000)
+    except:
+        send.stop()
+
+        raise
+
+if __name__ == '__main__':
+    main()
+
