d8152268f6cbe9e83035dde50b4b049bd301ffc3
Sean Purcell <me@seanp.xyz>
2018-03-12 02:30:32 -0400

    finish frontend mostly

--- static/webview/templates.js
+++ static/webview/templates.js
@@ -1,35 +1,31 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['cost'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper;
+templates['cost'] = template({"1":function(container,depth0,helpers,partials,data) {
+    return "ms-split";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<i class=\"ms ms-"
-    + container.escapeExpression(((helper = (helper = helpers.val || (depth0 != null ? depth0.val : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"val","hash":{},"data":data}) : helper)))
-    + " ms-cost ms-shadow\"></i>\n";
+    + container.escapeExpression(((helper = (helper = helpers.cost || (depth0 != null ? depth0.cost : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"cost","hash":{},"data":data}) : helper)))
+    + " ms-cost ms-shadow "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.hybrid : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\"></i>\n";
 },"useData":true});
 templates['row'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials.cost,depth0,{"name":"cost","hash":{"val":depth0},"data":data,"indent":"            ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+  return ((stack1 = container.invokePartial(partials.cost,depth0,{"name":"cost","data":data,"indent":"            ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<li id=\"row-"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" onclick=\""
-    + alias4(((helper = (helper = helpers.row_fun || (depth0 != null ? depth0.row_fun : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"row_fun","hash":{},"data":data}) : helper)))
-    + "("
-    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + ")\" class=\"cs-card-row list-group-item px-2\">\n    <span>\n        "
+    + "\" class=\"cs-card-row list-group-item px-2\">\n    <span>\n        "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "\n    </span>\n    <span style=\"float: right;\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.cost : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "        <span class=\"pl-1\">\n        <i id=\"share-"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" onclick=\""
-    + alias4(((helper = (helper = helpers.share_fun || (depth0 != null ? depth0.share_fun : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"share_fun","hash":{},"data":data}) : helper)))
-    + "("
-    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + ")\" class=\"material-icons\" style=\"vertical-align: middle;\">send</i>\n        </span>\n    </span>\n</li>\n";
+    + "\"\n            class=\"cs-send material-icons\" style=\"vertical-align: middle;\">send</i>\n        </span>\n    </span>\n</li>\n";
 },"usePartial":true,"useData":true});
 })();
--- static/webview/webview.js
+++ static/webview/webview.js
@@ -1,12 +1,66 @@
 var cards = [];
 
+function share_card(idx) {
+    // Upload attachment
+    $.ajax({
+        url: 'https://mtgbot.seanp.xyz/api/upload/',
+        method: 'POST',
+    });
+    var card = cards[idx];
+    var message = {
+        attachment: {
+            type: 'template',
+            payload: {
+                template_type: 'generic',
+            },
+        },
+    };
+    function build_element(card_face) {
+        return {
+            title: card_face.name,
+            image_url: card_face.image_uris.normal,
+            buttons: [{
+                type: 'web_url',
+                url: card.scryfall_uri,
+                title: 'Scryfall',
+            }]
+        };
+    }
+    if (card.card_faces === undefined)
+        message.attachment.payload.elements = [
+            build_element(card),
+        ];
+    else
+        message.attachment.payload.elements = card
+            .card_faces
+            .map(build_element);
+
+    MessengerExtensions.beginShareFlow(
+        function(share_response) {
+            if (share_response.is_sent)
+                MessengerExtensions.requestBrowserClose();
+        },
+        function(errorCode, errorMessage) {},
+        message,
+        'current_thread');
+}
+
 function row_click(idx) {
-    $(".cs-card-row").removeClass('active');
-    $("#row-" + idx).toggleClass('active');
+    console.log(idx);
+    return function(event) {
+        $(".cs-card-row").removeClass('active');
+        $("#row-" + idx).toggleClass('active');
+    }
 }
 
 function share_click(idx) {
-    console.log('share ' + idx);
+    return function(event) {
+        console.log('share ' + idx);
+
+        event.stopPropagation();
+
+        share_card(idx);
+    }
 }
 
 Handlebars.registerPartial('cost', Handlebars.templates.cost);
@@ -16,8 +70,6 @@ function build_card_row(card) {
             id: card.id,
             name: card.name,
             cost: card.cost,
-            row_fun: 'row_click',
-            share_fun: 'share_click',
         })
 }
 
@@ -26,9 +78,13 @@ function parse_manacost(cost) {
     var costs = cost.match(regex);
     return costs === null ? [] :
         costs.map(cost => cost
-        .slice(1, -1)
-        .replace('/', '')
-        .toLowerCase());
+                .slice(1, -1)
+                .toLowerCase())
+            .map(cost =>
+                ({
+                    cost: cost.replace('/', ''),
+                    hybrid: cost.indexOf('/') !== -1,
+                }));
 }
 
 function parse_card(card, idx) {
@@ -55,8 +111,10 @@ function update_list() {
     }
 
     cards.forEach(function(card, idx) {
-        list.append(build_card_row(
-            parse_card(card, idx)));
+        $(build_card_row(parse_card(card, idx)))
+            .appendTo(list)
+            .click(row_click(idx))
+            .find('.cs-send').click(share_click(idx));
     });
 }
 
@@ -76,20 +134,8 @@ function scryfall_search(query) {
     });
 }
 
-/*
-$("#card-list").append(
-    row_template({
-        id: 0,
-        name: 'Jace, the Mind Sculptor',
-        cost: ['2', 'u', 'u'],
-        row_fun: 'row_click',
-        share_fun: 'share_click',
-    })
-);
-*/
 $("#search-bar").submit(function() {
     var query = $("#search-text").val();
-    console.log(query);
     scryfall_search(query);
     document.activeElement.blur();
     return false;
