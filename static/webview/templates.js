7fe5a8322694c9deb8b46cb53afbe26b9e4bcd57
Sean Purcell <me@seanp.xyz>
2018-03-11 23:11:40 -0400

    Get basic webview working, just need to add share and collapse

--- static/webview/templates.js
+++ static/webview/templates.js
@@ -16,10 +16,20 @@ templates['row'] = template({"1":function(container,depth0,helpers,partials,data
 
   return "<li id=\"row-"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" class=\"list-group-item px-2\">\n    <span>\n        "
+    + "\" onclick=\""
+    + alias4(((helper = (helper = helpers.row_fun || (depth0 != null ? depth0.row_fun : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"row_fun","hash":{},"data":data}) : helper)))
+    + "("
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ")\" class=\"cs-card-row list-group-item px-2\">\n    <span>\n        "
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "\n    </span>\n    <span style=\"float: right;\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.cost : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "        <span class=\"pl-1\">\n        <i class=\"material-icons\" style=\"vertical-align: middle;\">send</i>\n        </span>\n    </span>\n</li>\n";
+    + "        <span class=\"pl-1\">\n        <i id=\"share-"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" onclick=\""
+    + alias4(((helper = (helper = helpers.share_fun || (depth0 != null ? depth0.share_fun : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"share_fun","hash":{},"data":data}) : helper)))
+    + "("
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ")\" class=\"material-icons\" style=\"vertical-align: middle;\">send</i>\n        </span>\n    </span>\n</li>\n";
 },"usePartial":true,"useData":true});
 })();
--- static/webview/webview.js
+++ static/webview/webview.js
@@ -1,18 +1,96 @@
-(function() {
-    $("#search-bar").submit(function() {
-        console.log($("#search-text").val());
-    });
+var cards = [];
 
-    Handlebars.registerPartial('cost', Handlebars.templates.cost);
+function row_click(idx) {
+    $(".cs-card-row").removeClass('active');
+    $("#row-" + idx).toggleClass('active');
+}
 
+function share_click(idx) {
+    console.log('share ' + idx);
+}
+
+Handlebars.registerPartial('cost', Handlebars.templates.cost);
+function build_card_row(card) {
     var row_template = Handlebars.templates.row;
-
-    $("#card-list").append(
-        row_template({
-            id: 0,
-            name: 'Jace, the Mind Sculptor',
-            cost: ['2', 'u', 'u'],
+    return row_template({
+            id: card.id,
+            name: card.name,
+            cost: card.cost,
+            row_fun: 'row_click',
+            share_fun: 'share_click',
         })
-    );
-    console.log('added');
-}());
+}
+
+function parse_manacost(cost) {
+    var regex = /{[WUBRG/0-9]*}/g;
+    var costs = cost.match(regex);
+    return costs === null ? [] :
+        costs.map(cost => cost
+        .slice(1, -1)
+        .replace('/', '')
+        .toLowerCase());
+}
+
+function parse_card(card, idx) {
+    var data = {
+        name: card.name,
+        id: idx,
+    };
+    var cost = (card.card_faces === undefined ?
+            card.mana_cost :
+            card.card_faces[0].mana_cost);
+    data.cost = parse_manacost(cost);
+
+    return data;
+}
+
+function update_list() {
+    var list = $('#card-list');
+
+    list.empty();
+    if (cards.length == 0) {
+        $("#no-results").show();
+    } else {
+        $("#no-results").hide();
+    }
+
+    cards.forEach(function(card, idx) {
+        list.append(build_card_row(
+            parse_card(card, idx)));
+    });
+}
+
+function scryfall_search(query) {
+    $.ajax({
+        url: 'https://api.scryfall.com/cards/search',
+        data: {
+            q: query,
+            include_extras: true,
+        }
+    }).done(function(resp) {
+        cards = resp.data;
+        update_list();
+    }).fail(function() {
+        cards = [];
+        update_list();
+    });
+}
+
+/*
+$("#card-list").append(
+    row_template({
+        id: 0,
+        name: 'Jace, the Mind Sculptor',
+        cost: ['2', 'u', 'u'],
+        row_fun: 'row_click',
+        share_fun: 'share_click',
+    })
+);
+*/
+$("#search-bar").submit(function() {
+    var query = $("#search-text").val();
+    console.log(query);
+    scryfall_search(query);
+    document.activeElement.blur();
+    return false;
+});
